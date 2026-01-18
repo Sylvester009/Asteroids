446fcc73bc82941492ea463087b09b8a6c85d814
Samuel Sylvester <samsylvester09@gmail.com>
2026-01-18 16:52:10 +0000

    setting up palyer and its movement

---
+++ main.py
@@ -0,0 +1,37 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+from player import Player
+
+def main():
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        log_state()
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
+        
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000.0
+
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
